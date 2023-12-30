4be10230170266820c2c6bb72a6ef9a79ed7c9ae
bp86 <155181310+bp86@users.noreply.github.com>
2023-12-30 18:41:45 -0500

    Update bp_friend_scanner.py
    Its finished :D
    
    It prints out the URL to all of your friends cuz i was too lazy to extract the username + userid (the URL includes both of them anyways)

--- bp_friend_scanner.py
+++ bp_friend_scanner.py
@@ -21,20 +21,42 @@ def checkIfUserExists(userId):
 
 
 if checkIfUserExists(userId):
-    pageNumber = 1
+    currentPage = 1
     
-    friendListURL = "https://www.brickplanet.com/profile/" + str(userId) + "/view-friends?page=" + str(pageNumber)
+    friendListURL = "https://www.brickplanet.com/profile/" + str(userId) + "/view-friends?page=" + str(currentPage)
     
     response = requests.get(friendListURL)
     soup = BeautifulSoup(response.text, "html.parser")
+
+    friendString = soup.find("div", class_ = "text-2xl fw-semibold mb-2").text # Example string: "86's Friends (57)"
+    characterPosition = 0 # The position of the "(" in "friendString"
+    numberOfFriends = ""
+
+    numberOfPages = 0
+
+
+    # Finds the number between the parethesis in "friendString"
+    for i in range(len(friendString) - 2, -1, -1): # length of string - 2 because we dont need the last charcter ")"
+        if friendString[i] == "(":
+            characterPosition = i
+            break
+
+    # Isolates the numbers between the parenthesis in "friendString"
+    for i in range(characterPosition + 1, len(friendString) - 1, 1):
+        numberOfFriends += friendString[i]
+
+    numberOfPages = int(int(numberOfFriends) / 18) + 1
+
     
-    allFriendsOnPage = soup.find_all("div", class_ = "col-4 col-lg-2 text-center")
-    #numberOfFriends = soup.find("div", class_ = "text-2xl fw-semibold mb-2").text
-    
-    for i in range(len(allFriendsOnPage)):
-        if i == 17: pageNumber += 1
-            
-    
-    
-    #col-4 col-lg-2 text-center
-    #truncate  text-light 
+    for i in range(numberOfPages):
+        currentPage = i + 1
+        friendListURL = "https://www.brickplanet.com/profile/" + str(userId) + "/view-friends?page=" + str(currentPage)
+
+        response = requests.get(friendListURL)
+        soup = BeautifulSoup(response.text, "html.parser")
+        allFriendsOnPage = soup.find_all("div", class_ = "col-4 col-lg-2 text-center")  # 6x3 players/friends on each page (18 players/friends per page)
+        
+        for playerHTML in allFriendsOnPage:
+            a_tag = playerHTML.find("a", class_ = "d-block truncate text-light")
+            friendURL = a_tag["href"] # its just the URL to the player/friend cuz im too lazy to extract both the username and userid
+            print(friendURL)
