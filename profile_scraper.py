3b74ade2b2b91c9ac236cbae2d003afb32243b6e
bp86 <155181310+bp86@users.noreply.github.com>
2024-04-10 21:16:31 -0400

    Create profile_scraper.py
    it took god 6 days to create the earth and the heavens and he took 1 night of rest from that
    
    it took be like 20 minutes to code this and i never rest cuz im always on that grind

---
+++ profile_scraper.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+userId = 1 # Change this your desired UserId
+
+url = f"https://www.brickplanet.com/profile/{userId}"
+
+
+response = requests.get(url)
+
+if response.status_code >= 200 and response.status_code <= 400: 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    username = soup.find("title").text.split("|")[0].strip()
+    bio = "N/A"
+    netWorth = "N/A"
+    rank = "N/A"
+
+    userStats = []
+    level = "N/A"
+    lastSeen = "N/A"
+    joinDate = "N/A"
+    profileViews = "N/A"
+    forumPosts = "N/A"
+
+
+    try: 
+        bio = soup.find("div", class_ = "card text-sm card-body mb-4").text.strip()
+        valueInfo = soup.find("span", class_ = "text-credits").text.strip().split("#")
+        netWorth = valueInfo[0].split("\n")[0]
+        rank = valueInfo[1]
+
+
+        for stat in soup.find("div", class_ = "d-flex flex-column gap-1 text-sm card card-body mb-4"):
+            userStats.append(stat.text.strip())
+
+        level = userStats[1]
+        lastSeen = userStats[3]
+        joinDate = userStats[5]
+        profileViews = userStats[7]
+        forumPosts = userStats[9]
+
+    except AttributeError:
+        pass
+    
+    
+    print(f"Username: {username} \nBio: {bio} \nNetWorth: {netWorth} \nRank: {rank}")
+    print(f"{level} \n{lastSeen} \n{joinDate} \n{profileViews} \n{forumPosts}")
+    
+else:
+    print("You've sent too many requests OR the BP servers are down.")
