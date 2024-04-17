b37e335656779a08ac6f1e121d5b1ef56afe54ce
86 <155181310+bp86@users.noreply.github.com>
2024-04-17 09:31:26 -0400

    i succ
    pp

--- rare_stats.py
+++ rare_stats.py
@@ -10,3 +10,9 @@ divThing = soup.find_all("div", class_ = "text-2xl fw-semibold text-credits mb-1
 estimatedValue = divThing[0].text.strip() # RAP
 volume = divThing[1].text.strip()
 marketCap = divThing[2].text.strip()
+activeCopies = soup.find("div", class_ = "text-2xl fw-semibold text-white mb-1").text.strip()
+
+print(f"\nEstimated Value: {estimatedValue}")
+print(f"Volume: {volume}")
+print(f"Market Cap: {marketCap}")
+print(f"Active Copies: {activeCopies}")
