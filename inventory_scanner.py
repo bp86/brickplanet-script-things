87a9d96113e699400de7a7ba1ad7862488ccd2e5
86 <155181310+bp86@users.noreply.github.com>
2025-02-12 16:48:42 -0500

    Update inventory_scanner.py
    optimized and easier to read print statements.
    
    previously: printed the items on each page
    
    now: prints every item at simultaneously with their value and how many copies the user has of that item. example: Ushanka x3   Value = 5853

--- inventory_scanner.py
+++ inventory_scanner.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+from time import sleep
 
 itemTypes = {
     0 : "AllItems",
@@ -18,115 +18,113 @@ itemTypes = {
     12 : "Models"
 }
 
-userId = 6731
-itemType = 4
-rarity = 1  # 0 is all items | 1 is only rares
-currentPage = 1 # dont change this
+userID = 0  # Change to your desired UserID
+itemType = 0  # Filters to only display items of this type. Number to item type above
+rarity = 0  # 0 is all items | 1 is only rares
 
-filterType = "Value"  # Filter types: Value (rarity must = 1 to filter by value) and Price
-valueFilter = 100 # Change this number to whatever you want. if you dont want a filter, make it = None
-priceFilter = 250 # Change this number to whatever you want. if you dont want a filter, make it = None
+filterType = "Value"  # Filter types: Price and Value (for rares only)
+valueFilter = 20 # Change this number to whatever you want. If you dont want a filter, make it = None
+priceFilter = 100 # Change this number to whatever you want. If you dont want a filter, make it = None
 
-goodList = [] # A list of items that the script will use to cach item names that meet the value/price requirement. makes the program run faster and saves isaac some money
-ignoreList = [] # A list of items that the script will use to cach item names that dont meet the value/price requirement. makes the program run faster and saves isaac some money
+userInventory = {} # The user's inventory
+ignoreList = {} # List that caches items that dont meet the value/price requirement
 
-inventoryURL = f"https://www.brickplanet.com/profile/{userId}/view-backpack?type={itemType}&page={currentPage}&rare={rarity}"
+inventoryURL = f"https://www.brickplanet.com/profile/{userID}/view-backpack?type={itemType}&page=1&rare={rarity}"
 
-    
-def checkStatusCode(url):
+
+def StatusCode(url):
     response = requests.get(url)
     return response.status_code >= 200 and response.status_code <= 400
-    
-def checkIfUserExists(userId):
-    url = f"https://www.brickplanet.com/profile/{userId}"
+
+def UserExists(userID):
+    url = f"https://www.brickplanet.com/profile/{userID}"
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     title = soup.find("title").text
-    
-    return checkStatusCode(url) and not "Players" in title
 
-def filterNumber(num : str):
+    return StatusCode(url) and "Players" not in title
+
+def FilterNumber(num : str):
     filtered = ""
     abbreviationMultiplier = 1
 
     for char in num.strip():
-        if char == "M": abbreviationMultiplier = 1000000
-        if char.isdigit() or char == ".": filtered += char
+        if char == "M":
+            abbreviationMultiplier = 1000000
+
+        if char.isdigit() or char == ".":
+            filtered += char
 
     return float(filtered) * abbreviationMultiplier
 
 
+if UserExists(userID):
 
-if checkIfUserExists(userId):
-    
+    currentPage = 1
+
     while True:
-        
-        inventoryURL = f"https://www.brickplanet.com/profile/{userId}/view-backpack?type={itemType}&page={currentPage}&rare={rarity}"
-        
+
+        inventoryURL = f"https://www.brickplanet.com/profile/{userID}/view-backpack?type={itemType}&page={currentPage}&rare={rarity}"
+
         content = requests.get(inventoryURL).text
         soup = BeautifulSoup(content, "html.parser")
-        
+
         allItemsOnPage = soup.find_all("div", class_ = "col-6 col-md-3")
 
-        if soup.find_all("div", class_ = "faded"): break # If no items found on the page
-        
+        if soup.find_all("div", class_ = "faded"): # If no items found on the page
+            break
+
         if not allItemsOnPage:
             print("Connection error. Either BP servers are down or you've sent too many requests. Program will resume in 10 seconds.")
             currentPage -= 1
-            time.sleep(10)        
-        
-        print("\nCurrent Page:", currentPage)
+            sleep(10)        
 
         for item in allItemsOnPage:
             itemContent = item.find("a", class_ = "d-block truncate text-decoration-none fw-semibold text-light mb-1")
             itemName = itemContent.text.strip()
 
-            if itemName in ignoreList: continue
+            if itemName in ignoreList:
+                continue
+
+            if itemName in userInventory:
+                userInventory[itemName][0] += 1 # Increment the quantity
+                continue
 
             if filterType.lower() == "price":
                 price = None
 
                 if itemContent.find("div", class_ = "text-credits") != None:
-                    price = filterNumber(item.find("div", class_ = "text-credits").text.strip())
-                
-                if itemName in goodList:
-                    print(f"{itemName} : {price}")
-                elif priceFilter == None or price == None:
-                    print(f"{itemName} : {price}")
-                    goodList.append(itemName)
-                elif price <= priceFilter: # Filter can be '<=' or '>='
-                    print(f"{itemName} : {price}")
-                    goodList.append(itemName)
-                else:
-                    ignoreList.append(itemName)
+                    price = FilterNumber(item.find("div", class_ = "text-credits").text.strip())
 
-            elif (filterType.lower() == "value" or filterType.lower() == "rap") and rarity == 1:
+                if (priceFilter == None) or (price == None) or (price <= priceFilter): # priceFilter can be '<=' or '>='
+                    userInventory[itemName] = [1, price] # [Quantity, price]
+                else:
+                    ignoreList[itemName] = True
+
+            elif (filterType.lower() == "value" or filterType.lower() == "rap") and (rarity == 1):
                 itemLink = itemContent["href"]
                 content = requests.get(itemLink).text
                 soup = BeautifulSoup(content, "html.parser")
                 divThing = soup.find_all("div", class_ = "text-2xl fw-semibold text-credits mb-1")
 
-                estimatedValue = filterNumber(divThing[0].text.strip())
+                estimatedValue = FilterNumber(divThing[0].text.strip())
 
-                if itemName in goodList:
-                    print(f"{itemName} : {estimatedValue}")
-                elif valueFilter == None:
-                    print(f"{itemName} : {estimatedValue}")
-                    goodList.append(itemName)
-                elif estimatedValue >= valueFilter:
-                    print(f"{itemName} : {estimatedValue}")
-                    goodList.append(itemName)
+                if (valueFilter == None) or (estimatedValue >= valueFilter): # valueFilter can be '<=' or '>='
+                   userInventory[itemName] = [1, estimatedValue] # [Quantity, estiimatedValue]
                 else:
-                    ignoreList.append(itemName)
+                    ignoreList[itemName] = True
 
             else:
                 print("Invalid item type")
                 break
-        
+
         currentPage += 1
-    
-    print("\nScanning Complete")
-    
+
+    print("\n")
+
+    for item in userInventory:
+        print(f"{item} x{userInventory[item][0]}    {filterType} = {userInventory[item][1]}")
+
 else:
     print("Player doesn't exist OR you've sent too many requests OR the BP servers are down.")
